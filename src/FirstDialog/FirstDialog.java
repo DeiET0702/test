0308e8aa9b130e0802ba120f60aba4475ef09c2c
DeiET0702 <extra-terrestrial@MacBook-Pro-cua-Pham.local>
2024-05-08 16:02:21 +0700

    First update

---
+++ src/FirstDialog/FirstDialog.java
@@ -0,0 +1,10 @@
+package FirstDialog;
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hell world! How are you?");
+		System.exit(0);
+	}
+
+}
---
+++ src/ShowTwoNumbers/ShowTwoNumbers.java
@@ -0,0 +1,5 @@
+package ShowTwoNumbers;
+
+public class ShowTwoNumbers {
+
+}
